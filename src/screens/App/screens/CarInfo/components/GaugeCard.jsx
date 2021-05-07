9fdfab707f66e3c99bc1425ddbb4c077637a6c88
Frank Blechschmidt <contact@frank-blechschmidt.com>
2021-05-06 23:08:11 -0700

    Clean up GaugeCard

--- src/screens/App/screens/CarInfo/components/GaugeCard.jsx
+++ src/screens/App/screens/CarInfo/components/GaugeCard.jsx
@@ -24,46 +24,18 @@ const GaugeCard = ({ name, value, valuePct, unit, icon }) => {
           />
           <Box direction="row" align="center" pad={{ bottom: "xsmall" }}>
             <Text size="xlarge" weight="bold">
-              {numbro(value).format({
-                thousandSeparated: true,
-                mantissa: 0,
-              })}
+              {value
+                ? numbro(value).format({
+                    thousandSeparated: true,
+                    mantissa: 0,
+                  })
+                : "N/A"}
             </Text>
             <Text size="small">{unit}</Text>
           </Box>
         </Stack>
       </Box>
     </Card>
-    // <Card pad="medium" direction="row" gap="large" background="white" round>
-    //   <Box gap="small" fill>
-    //     <Box direction="row" gap="medium">
-    //       {icon}
-    //       <Heading level="2" margin="none" size="small">
-    //         {name}
-    //       </Heading>
-    //     </Box>
-    //     <Stack anchor="center">
-    //       <Meter
-    //         round
-    //         type="circle"
-    //         size="small"
-    //         background="light-2"
-    //         color="focus"
-    //         value={valuePct}
-    //         thickness="small"
-    //       />
-    //       <Box direction="row" align="center" pad={{ bottom: "xsmall" }}>
-    //         <Text size="xlarge" weight="bold">
-    //           {numbro(value).format({
-    //             thousandSeparated: true,
-    //             mantissa: 1,
-    //           })}
-    //         </Text>
-    //         <Text size="small">{unit}</Text>
-    //       </Box>
-    //     </Stack>
-    //   </Box>
-    // </Card>
   );
 };
 
